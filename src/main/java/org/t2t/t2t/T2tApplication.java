f19ec9a384c24c6bd21b85e7f2cc02c8b126c164
HongSaek-Ko <bh1152@naver.com>
2024-11-15 18:12:33 +0900

    최초 commit

---
+++ src/main/java/org/t2t/t2t/T2tApplication.java
@@ -0,0 +1,13 @@
+package org.t2t.t2t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2tApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2tApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/t2t/t2t/T2tApplicationTests.java
@@ -0,0 +1,13 @@
+package org.t2t.t2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2tApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
