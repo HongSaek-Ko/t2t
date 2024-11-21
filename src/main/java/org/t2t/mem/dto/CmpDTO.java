b5885ebf33d6b9cac5fa1e53c4f25e2217124bd8
zzangnaa <skdud0369@naver.com>
2024-11-21 20:03:02 +0900

    마이페이지 수정

---
+++ src/main/java/org/t2t/mem/dto/CmpDTO.java
@@ -0,0 +1,21 @@
+package org.t2t.mem.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class CmpDTO {
+    private int cmpId;
+    private String usrId;
+    private int prdId;
+    private String cmpCateCd;
+    private String cmpStat;
+    private String admId;
+    private String cRstCd;
+    private LocalDateTime regDt;
+    private LocalDateTime lastDt;
+
+}
