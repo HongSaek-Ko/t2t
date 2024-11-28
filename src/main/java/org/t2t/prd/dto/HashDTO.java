1a198d3009578f4f92f8986c8f2756aad67362a2
HongSaek-Ko <bh1152@naver.com>
2024-11-29 02:19:21 +0900

    쿼리문 추가 (임시)

--- src/main/java/org/t2t/prd/dto/HashDTO.java
+++ src/main/java/org/t2t/prd/dto/HashDTO.java
@@ -11,7 +11,7 @@ public class HashDTO {
     private char enabled; // 사용 가능 여부(Y/N), default Y, 셀렉트 시 enabled Y만 노출
     private LocalDateTime regDt;
     private LocalDateTime lastDt;
-
+    // 관리자가 관리 (사용자는 미리 입력된 해시태그 테이블에서 데이터 불러옴(자동완성)
 
 
 }
