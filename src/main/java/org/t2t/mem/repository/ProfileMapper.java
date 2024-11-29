cfd42fe9130e47d2f046efa275ff6aa446771e02
zzangnaa <skdud0369@naver.com>
2024-11-29 16:22:59 +0900

    Profile Mapper 관련 xml 수정

--- src/main/java/org/t2t/mem/repository/ProfileMapper.java
+++ src/main/java/org/t2t/mem/repository/ProfileMapper.java
@@ -6,6 +6,7 @@ import org.t2t.mem.dto.ProfileDTO;
 @Mapper
 public interface ProfileMapper {
     // 파일 정보 저장
-    public int insertFile(ProfileDTO imgProfileDTO);
+    void insertFile(ProfileDTO imgProfileDTO);
 
+    void updateProfile(ProfileDTO profileDTO);
 }
